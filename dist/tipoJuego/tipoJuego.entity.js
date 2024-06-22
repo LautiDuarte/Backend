29247e8edf66070b52468bbe4d3bcd0aa73308f9
Duarte, Lautaro <164930302+LautiDuarte@users.noreply.github.com>
2024-06-22 19:27:06 -0300

    Delete dist directory

--- dist/tipoJuego/tipoJuego.entity.js
+++
@@ -1,9 +0,0 @@
-import crypto from 'node:crypto';
-export class TipoJuego {
-    constructor(name, description, id = crypto.randomUUID()) {
-        this.name = name;
-        this.description = description;
-        this.id = id;
-    }
-}
-//# sourceMappingURL=tipoJuego.entity.js.map
