d6077541cadbc5528f6d546d4b83c8fcc3cd03cd
Leo Carovana <leonelcarovana11@gmail.com>
2024-09-03 20:20:54 -0300

    Cambios en el back para los usuarios y el login

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,8 @@
 import { User } from './user.entity.js'
 import { Request, Response, NextFunction } from 'express'
 import { orm } from '../shared/db/orm.js'
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken'
 
 const em = orm.em
 
@@ -8,12 +10,12 @@ function sanitizeuserInput(req: Request, res: Response, next: NextFunction) {
   req.body.sanitizedInput = {
     name: req.body.name,
     lastName: req.body.lastName,
-    alias: req.body.alias,
+    userName: req.body.userName,
     email: req.body.email,
-    password: req.body.password,
+    //eliminé la contraseña para despues guardarla ya hasheada
     teams: req.body.teams,
-    competitionsCreated: req.body.competitionsCreated
-  }
+    competitionsCreated: req.body.competitionsCreated,
+  };
 
 
   Object.keys(req.body.sanitizedInput).forEach((key) => {
@@ -31,7 +33,7 @@ async function findAll(req: Request, res: Response) {
       {},
       { populate: ['competitionsCreated', 'teams'] }
     )
-    res.status(200).json({ message: 'found all users', data: users })
+    res.status(200).json(users)
   } catch (error: any) {
     res.status(500).json({ message: error.message })
   }
@@ -48,22 +50,51 @@ async function findOne(req: Request, res: Response) {
     )
     res
       .status(200)
-      .json({ message: 'found user', data: user })
+      .json(user)
   } catch (error: any) {
     res.status(500).json({ message: error.message })
   }
 }
 
-async function add(req: Request, res: Response) {
-  try {
-    const user = em.create(User, req.body.sanitizedInput)
-    await em.flush()
-    res
-      .status(201)
-      .json({ message: 'user created', data: user })
+async function newUser(req:Request, res:Response) {
+  try{
+    const { userName } = req.body;
+
+    //validamos si el usuario ya existe en la base de datos
+    const existingUser = await em.findOne(User, { userName });
+    if (existingUser){
+      return res
+        .status(500)
+        .json({ message: `Ya existe un usuario con el userName ${userName}` });
+    }  
+
+    // Si no existe, procedemos con la creación del usuario
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const user = em.create(User, { ...req.body.sanitizedInput, password: hashedPassword});
+    await em.flush();
+    res.status(201).json(user);
   } catch (error: any) {
-    res.status(500).json({ message: error.message })
+    res.status(500).json({ message: error.message });
   }
+}
+
+async function loginUser(req: Request, res: Response) {
+  const { userName, password } = req.body;
+  //validamos si el usuario ya existe en la base de datos
+  const existingUser: any = await em.findOne(User, { userName });
+  if (!existingUser) {
+    return res.status(500).json({ message: `No existe un usuario con el userName ${userName}` });
+  }
+
+  //validamos password
+  const passwordValid = await bcrypt.compare(password, existingUser.password);
+  if (!passwordValid){
+    return res.status(500).json({ message: `Password Incorrecta` });
+  }
+
+  //generamos token
+  const token = jwt.sign({ userName: userName }, process.env.SECRET_KEY || 'pepito123');
+  res.json({token});
 }
 
 async function update(req: Request, res: Response) {
@@ -72,7 +103,7 @@ async function update(req: Request, res: Response) {
     const user = em.getReference(User, id)
     em.assign(user, req.body.sanitizedInput)
     await em.flush()
-    res.status(200).json({ message: 'user updated' })
+    res.status(200).json()
   } catch (error: any) {
     res.status(500).json({ message: error.message })
   }
@@ -83,10 +114,18 @@ async function remove(req: Request, res: Response) {
     const id = Number.parseInt(req.params.id)
     const user = em.getReference(User, id)
     await em.removeAndFlush(user)
-    res.status(200).send({ message: 'user deleted' })
+    res.status(200).send()
   } catch (error: any) {
     res.status(500).json({ message: error.message })
   }
 }
 
-export { sanitizeuserInput, findAll, findOne, add, update, remove }
+export {
+  sanitizeuserInput,
+  findAll,
+  findOne,
+  newUser,
+  loginUser,
+  update,
+  remove,
+};
